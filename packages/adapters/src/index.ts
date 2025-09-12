bf8abf6811b652a5eff0965a7ef970dc4907846e
architect-dev <halftonedev@gmail.com>
2025-09-12 13:21:47 +0200

    Fix types, remove dead code

--- packages/adapters/src/index.ts
+++ packages/adapters/src/index.ts
@@ -1,24 +1,5 @@
-// Simple Adapters - take signer and provider, return viem clients
-export { 
-  Ethers5Adapter,
-  type Ethers5AdapterResult,
-  type Ethers5AdapterConfig,
-} from './ethers5'
-
-export { 
-  Ethers6Adapter,
-  type Ethers6AdapterResult,
-  type Ethers6AdapterConfig,
-} from './ethers6'
-
-
-export { 
-  WagmiAdapter,
-  type WagmiAdapterResult,
-} from './wagmi'
-
-export { 
-  HardhatSignerAdapter,
-  type HardhatAdapterResult,
-  type HardhatAdapterConfig,
-} from './hardhat'
+export { Ethers5Adapter } from './ethers5'
+export { Ethers6Adapter } from './ethers6'
+export { WagmiAdapter } from './wagmi'
+export { HardhatSignerAdapter } from './hardhat'
+export type { AdapterResult } from './types'
