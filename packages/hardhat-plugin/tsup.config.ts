30bef1c0b453b3dd148a1669480edd42033cde97
architect-dev <halftonedev@gmail.com>
2025-10-16 18:02:11 +0200

    Remove tests (migrated to own pkg), use exports from mock-contracts

--- packages/hardhat-plugin/tsup.config.ts
+++ packages/hardhat-plugin/tsup.config.ts
@@ -1,16 +1,11 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
-  external: [
-    "hardhat",
-    "@nomicfoundation/hardhat-ethers",
-    "ethers",
-    "@cofhesdk/node",
-  ],
+  external: ['hardhat', '@nomicfoundation/hardhat-ethers', 'ethers'],
 });
