c40cf0c0405b5854e2b1342c76ba5e3df843d606
Alexey Shchur <hi@alexeyshchur.com>
2025-11-21 15:41:40 +0200

    fix: only init iframe-shared-storage on creating web storage, not upfront

--- packages/sdk/web/storage.ts
+++ packages/sdk/web/storage.ts
@@ -5,23 +5,22 @@ import { constructClient } from 'iframe-shared-storage';
  * @returns IStorage implementation for browser environments
  */
 
-const client = constructClient({
-  iframe: {
-    src: 'https://iframe-shared-storage.vercel.app/hub.html',
-    messagingOptions: {
-      enableLog: 'both',
-    },
-    iframeReadyTimeoutMs: 1000, // if the iframe is not initied during this interval AND a reuqest is made, such request will throw an error
-  },
-});
-
-const indexedDBKeyval = client.indexedDBKeyval;
-
-if (!indexedDBKeyval) {
-  throw new Error('IndexedDBKeyval is not available in the client');
-}
-
 export const createWebStorage = (): IStorage => {
+  const client = constructClient({
+    iframe: {
+      src: 'https://iframe-shared-storage.vercel.app/hub.html',
+      messagingOptions: {
+        enableLog: 'both',
+      },
+      iframeReadyTimeoutMs: 1000, // if the iframe is not initied during this interval AND a reuqest is made, such request will throw an error
+    },
+  });
+
+  const indexedDBKeyval = client.indexedDBKeyval;
+
+  if (!indexedDBKeyval) {
+    throw new Error('IndexedDBKeyval is not available in the client');
+  }
   return {
     getItem: async (name: string) => {
       try {
