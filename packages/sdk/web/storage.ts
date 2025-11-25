0b531fe04fbbdc36afb0d3e2837d1062139d0871
Alexey Shchur <hi@alexeyshchur.com>
2025-11-25 21:28:35 +0200

    redundantly increase iframeReadyTimeoutMs (blind guess since there's no error in logs)

--- packages/sdk/web/storage.ts
+++ packages/sdk/web/storage.ts
@@ -11,7 +11,7 @@ export const createWebStorage = (): IStorage => {
       messagingOptions: {
         enableLog: 'both',
       },
-      iframeReadyTimeoutMs: 1000, // if the iframe is not initied during this interval AND a reuqest is made, such request will throw an error
+      iframeReadyTimeoutMs: 15_000, // if the iframe is not initied during this interval AND a reuqest is made, such request will throw an error
     },
   });
 
