2f219312a11ba048480737a9684e9d0bed5d2a4f
Alexey Shchur <hi@alexeyshchur.com>
2025-11-25 18:00:50 +0200

    a bit more verbose tests

--- packages/sdk/web/encryptInputs.web.test.ts
+++ packages/sdk/web/encryptInputs.web.test.ts
@@ -83,7 +83,12 @@ describe('@cofhe/web - Encrypt Inputs Browser Tests', () => {
     it('should encrypt a bool with real TFHE in browser', async () => {
       await cofhesdkClient.connect(publicClient, walletClient);
 
-      const result = await cofhesdkClient.encryptInputs([Encryptable.bool(true)]).encrypt();
+      const result = await cofhesdkClient
+        .encryptInputs([Encryptable.bool(true)])
+        .setStepCallback((step, context) => {
+          console.log('should encrypt a bool with real TFHE in browser - Step:', step, context);
+        })
+        .encrypt();
       const encrypted = expectResultSuccess(result);
 
       expect(encrypted.length).toBe(1);
