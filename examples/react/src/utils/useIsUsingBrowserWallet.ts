5da799d07e6be576f38b85783bddeb8da5c3ea46
Alexey Shchur <hi@alexeyshchur.com>
2025-12-05 10:15:21 +0200

    fix: use corret 'isBrowserWalletCOnnected' to FHE check

--- examples/react/src/utils/useIsUsingBrowserWallet.ts
+++ examples/react/src/utils/useIsUsingBrowserWallet.ts
@@ -1,21 +1,10 @@
-import { useCofheContext } from '@cofhe/react';
-import { usePublicClient, useWalletClient } from 'wagmi';
+import { useCofheWalletClient } from '@cofhe/react';
+import { useWalletClient } from 'wagmi';
 
 export function useIsUsingBrowserWallet() {
-  const wagmiPublicClient = usePublicClient();
   const { data: wagmiWalletClient } = useWalletClient();
+  const cofheWalletClient = useCofheWalletClient();
 
-  const cofhePublicClient = useCofheContext().client.getPublicClient();
-  const cofheWalletClient = useCofheContext().client.getWalletClient();
-
-  console.log({
-    wagmiPublicClient,
-    cofhePublicClient,
-    wagmiWalletClient,
-    cofheWalletClient,
-    'wagmi === cofhe public': wagmiPublicClient === cofhePublicClient,
-    'wagmi === cofhe wallet': wagmiWalletClient === cofheWalletClient,
-  });
-
+  // the true criteria is that both wallet clients are the same instance
   return wagmiWalletClient === cofheWalletClient;
 }
