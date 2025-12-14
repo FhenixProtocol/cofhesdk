f3000ddf97753cb31fdabd47fe3f8ad7c11094f5
Tovi Levis <tovi@fhenix.io>
2025-12-14 12:32:19 +0200

    Add card component

---
+++ packages/react/src/components/FnxFloatingButton/components/Card.tsx
@@ -0,0 +1,10 @@
+import { cn } from '../../../utils/cn.js';
+
+export const Card: React.FC<{
+  children: React.ReactNode;
+  className?: string;
+}> = ({ children, className }) => {
+  return <div className={cn('fnx-card-bg p-3 border fnx-card-border', className)}>{children}</div>;
+};
+
+
