683cafa7a126382403aeee3dd5fef8f376de90db
Alexey Shchur <hi@alexeyshchur.com>
2025-12-19 12:01:22 +0200

    chore: utilize newly created hook in the example

--- examples/react/src/utils/useAutoConnectCofhe.ts
+++ examples/react/src/utils/useAutoConnectCofhe.ts
@@ -1,28 +1,36 @@
-import { useCofheContext } from '@cofhe/react';
-import { useEffect } from 'react';
 import { useAccount, usePublicClient, useWalletClient } from 'wagmi';
 import { sepolia } from 'wagmi/chains';
 import { createMockWalletAndPublicClient } from './misc';
+import { useMemo } from 'react';
+import { useCofheAutoConnect } from '@cofhe/react';
 
 const DEFAULT_CHAIN_ID = sepolia.id;
 
-export const useAutoConnectCofhe = () => {
+/**
+ *
+ * @returns wagmi wallet if connected, otherwise hardcoded "test wallet" { publicClient: PublicClient; walletClient: WalletClient }
+ */
+const usePairForCofhe = () => {
   // TODO: if the user switches in the wallet to a chain that's not supported by the dapp, should show error message or disconnect?
-  const cofheClient = useCofheContext().client;
   const { isConnected: isWagmiConnected } = useAccount();
-  const publicClient = usePublicClient();
-  const { data: walletClient } = useWalletClient();
+  const wagmiPublicClient = usePublicClient();
+  const { data: wagmiWalletClient } = useWalletClient();
 
-  useEffect(() => {
-    const autoConnect = async () => {
-      if (isWagmiConnected && publicClient && walletClient) {
-        await cofheClient.connect(publicClient, walletClient);
-      } else {
-        // in case of no connected wallet, reset to mock wallet on a default chain
-        const { walletClient, publicClient } = createMockWalletAndPublicClient(DEFAULT_CHAIN_ID);
-        await cofheClient.connect(publicClient, walletClient);
-      }
-    };
-    autoConnect();
-  }, [isWagmiConnected, publicClient, walletClient, cofheClient]);
+  const { publicClient, walletClient } = useMemo(() => {
+    if (isWagmiConnected && wagmiPublicClient && wagmiWalletClient) {
+      return {
+        publicClient: wagmiPublicClient,
+        walletClient: wagmiWalletClient,
+      };
+    }
+    // in case of no connected wallet, reset to mock wallet on a default chain
+    return createMockWalletAndPublicClient(DEFAULT_CHAIN_ID);
+  }, [isWagmiConnected, wagmiPublicClient, wagmiWalletClient]);
+
+  return { publicClient, walletClient };
+};
+
+export const useAutoConnectCofhe = () => {
+  const { publicClient, walletClient } = usePairForCofhe();
+  useCofheAutoConnect({ publicClient, walletClient });
 };
