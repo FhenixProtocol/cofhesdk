c0dc9fe848c950f43fde6e808939847c67f33209
Alexey Shchur <hi@alexeyshchur.com>
2025-12-22 13:43:10 +0200

    fix: wagmi's reconnectOnMount issue

--- examples/react/src/utils/wagmi.tsx
+++ examples/react/src/utils/wagmi.tsx
@@ -17,12 +17,18 @@ const config = createConfig({
   syncConnectedChain: true,
 });
 
-const qc = new QueryClient();
+const wagmiQueryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={qc}>{children}</QueryClientProvider>
+    <WagmiProvider
+      config={config}
+      // TMP/TODO: had to disable reconnectOnMount to fix the problem with failed reconnecting even on button click.
+      // possible cause: the way the example app works (i.e. prevent eager connect) -- we don't pass injected() connector into config.connectors, instead we pass it at the wagm connection time (on button click)
+      // probably that's why it fails to reconnect
+      reconnectOnMount={false}
+    >
+      <QueryClientProvider client={wagmiQueryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
   );
 }
