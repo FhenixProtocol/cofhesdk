05e06eccaf3ab4964ca2781763cd56ae4f597461
Alexey Shchur <hi@alexeyshchur.com>
2026-01-12 09:10:19 +0200

    chore: react example: go with default config

--- examples/react/src/utils/cofhe.config.tsx
+++ examples/react/src/utils/cofhe.config.tsx
@@ -1,21 +1,9 @@
-import { CofheProvider, createCofhesdkConfig } from '@cofhe/react';
+import { CofheProvider } from '@cofhe/react';
 import { useClientsForCofheConnection } from './useClientsForCofheConnection';
-import { baseSepolia, sepolia } from '@cofhe/sdk/chains';
-
-const cofhesdkConfig = createCofhesdkConfig({
-  supportedChains: [sepolia, baseSepolia],
-  // useWorkers: true, // Enable Web Workers
-  react: {
-    recordTransactionHistory: true, // Enable activity page in floating button
-    // pinnedTokens: {
-    //   11155111: '0xd38AB9f1563316DeD5d3B3d5e727d55f410d492E',
-    // },
-  },
-});
 export const CofheProviderLocal = ({ children }: { children: React.ReactNode }) => {
   const { walletClient, publicClient } = useClientsForCofheConnection();
   return (
-    <CofheProvider config={cofhesdkConfig} walletClient={walletClient} publicClient={publicClient}>
+    <CofheProvider walletClient={walletClient} publicClient={publicClient}>
       {children}
     </CofheProvider>
   );
