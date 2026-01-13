5cedb96afab5db6e61c433482397848a59c3e911
Alexey Shchur <hi@alexeyshchur.com>
2026-01-13 09:16:38 +0200

    chore: add query debug to react example app

--- examples/react/src/utils/cofhe.config.tsx
+++ examples/react/src/utils/cofhe.config.tsx
@@ -1,10 +1,18 @@
-import { CofheProvider } from '@cofhe/react';
+import { CofheProvider, useInternalQueryClient } from '@cofhe/react';
 import { useClientsForCofheConnection } from './useClientsForCofheConnection';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+function QueryDebug() {
+  const cofheQueryClient = useInternalQueryClient();
+  return <ReactQueryDevtools client={cofheQueryClient} position="left" buttonPosition="bottom-left" />;
+}
+
 export const CofheProviderLocal = ({ children }: { children: React.ReactNode }) => {
   const { walletClient, publicClient } = useClientsForCofheConnection();
   return (
     <CofheProvider walletClient={walletClient} publicClient={publicClient}>
       {children}
+      <QueryDebug />
     </CofheProvider>
   );
 };
