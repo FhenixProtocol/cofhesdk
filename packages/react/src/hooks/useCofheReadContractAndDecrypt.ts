62ed8bc992a3637a29e4ad5964b7cf15e03a3a59
Alexey Shchur <hi@alexeyshchur.com>
2026-01-19 16:22:28 +0200

    fix: typing to account for ABI

--- packages/react/src/hooks/useCofheReadContractAndDecrypt.ts
+++ packages/react/src/hooks/useCofheReadContractAndDecrypt.ts
@@ -1,20 +1,26 @@
 import { type UseQueryOptions, type UseQueryResult } from '@tanstack/react-query';
-import { type Address, type Abi } from 'viem';
-import { FheTypes } from '@cofhe/sdk';
+import { type Address, type Abi, type ContractFunctionName, type ContractFunctionArgs } from 'viem';
+import { FheTypes, type UnsealedItem } from '@cofhe/sdk';
 import { ErrorCause } from '@/utils/errors';
 import { useCofheDecrypt } from './useCofheDecrypt';
-import { useCofheReadContract, type UseCofheReadContractQueryOptions } from './useCofheReadContract';
+import { useCofheReadContract, type InferredData, type UseCofheReadContractQueryOptions } from './useCofheReadContract';
+import { assert } from 'ts-essentials';
 
 /**
  * Generic hook: read a confidential contract value and decrypt it.
  */
-export function useCofheReadContractAndDecrypt<TDecryptedSelectedData = bigint>(
+export function useCofheReadContractAndDecrypt<
+  TFheType extends FheTypes,
+  TAbi extends Abi,
+  TfunctionName extends ContractFunctionName<TAbi, 'pure' | 'view'>,
+  TDecryptedSelectedData = InferredData<TAbi, TfunctionName>,
+>(
   params: {
     address?: Address;
-    abi?: Abi;
-    functionName?: string;
-    args?: readonly unknown[];
-    fheType: FheTypes;
+    abi?: TAbi;
+    functionName?: TfunctionName;
+    args?: ContractFunctionArgs<TAbi, 'pure' | 'view', TfunctionName>;
+    fheType: TFheType;
     requiresPermit?: boolean;
     potentialDecryptErrorCause: ErrorCause;
   },
@@ -23,24 +29,31 @@ export function useCofheReadContractAndDecrypt<TDecryptedSelectedData = bigint>(
     readQueryOptions,
     decryptingQueryOptions,
   }: {
-    readQueryOptions?: UseCofheReadContractQueryOptions;
+    readQueryOptions?: UseCofheReadContractQueryOptions<TAbi, TfunctionName>;
     decryptingQueryOptions?: Omit<
-      UseQueryOptions<string | bigint | boolean, Error, TDecryptedSelectedData>,
+      UseQueryOptions<UnsealedItem<TFheType>, Error, TDecryptedSelectedData>,
       'queryKey' | 'queryFn'
     >;
   } = {}
-): {
-  encrypted: UseQueryResult<bigint, Error>;
-  decrypted: UseQueryResult<TDecryptedSelectedData, Error>;
-  disabledDueToMissingPermit: boolean;
-} {
+) {
+  // : {
+  //   encrypted: UseQueryResult<bigint, Error>;
+  //   decrypted: UseQueryResult<TDecryptedSelectedData, Error>;
+  //   disabledDueToMissingPermit: boolean;
+  // }
   const { address, abi, functionName, args, fheType, requiresPermit = true, potentialDecryptErrorCause } = params;
 
   const encrypted = useCofheReadContract({ address, abi, functionName, args, requiresPermit }, readQueryOptions);
 
-  const decrypted = useCofheDecrypt(
+  const ciphertext = encrypted.data;
+
+  assert(typeof ciphertext === 'bigint', 'Expected ciphertext to be bigint or undefined');
+
+  let c = ciphertext;
+
+  const decrypted = useCofheDecrypt<TFheType, TDecryptedSelectedData>(
     {
-      ciphertext: encrypted.data,
+      ciphertext,
       fheType,
 
       cause: potentialDecryptErrorCause,
