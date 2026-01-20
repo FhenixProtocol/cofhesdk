b075c48ee1d0cc653398a0aa58512e842b8ddaff
Alexey Shchur <hi@alexeyshchur.com>
2026-01-20 10:25:32 +0200

    chore: cleanup

--- packages/react/src/utils/index.ts
+++ packages/react/src/utils/index.ts
@@ -3,5 +3,4 @@ export * from './utils';
 export * from './logging';
 export * from './debounce';
 export * from './errors';
-export * from './waitUntilBlockNumber';
 export * from './waitUntilRpcAwareAndReadContract';
