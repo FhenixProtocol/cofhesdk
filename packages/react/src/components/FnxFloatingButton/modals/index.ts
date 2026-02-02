bf951287f1aa9c4ae650767a301c2120616a1e63
architect-dev <halftonedev@gmail.com>
2026-02-02 14:27:31 +0100

    Add permit type explanation modal and embed in permit details modal

--- packages/react/src/components/FnxFloatingButton/modals/index.ts
+++ packages/react/src/components/FnxFloatingButton/modals/index.ts
@@ -2,9 +2,11 @@ import { ExampleSelectionPage } from './ExampleSelectionPage';
 import { ExampleInfoPage } from './ExampleInfoPage';
 import { PortalModal, type PortalModalStateMap } from './types';
 import { PermitDetailsModal } from './PermitDetailsModal';
+import { PermitTypeExplanationModal } from './PermitTypeExplanationModal';
 
 export const modals: { [M in PortalModal]: React.FC<PortalModalStateMap[M]> } = {
   [PortalModal.ExampleSelection]: ExampleSelectionPage,
   [PortalModal.ExampleInfo]: ExampleInfoPage,
   [PortalModal.PermitDetails]: PermitDetailsModal,
+  [PortalModal.PermitTypeExplanation]: PermitTypeExplanationModal,
 };
