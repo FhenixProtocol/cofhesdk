c5e693674973887b203c94bf8c329b08a8d37ea0
Alexey Shchur <hi@alexeyshchur.com>
2026-02-10 11:50:33 +0200

    fix: shield page, stuck selected token

--- packages/react/src/components/FnxFloatingButton/pages/ShieldPageV2.tsx
+++ packages/react/src/components/FnxFloatingButton/pages/ShieldPageV2.tsx
@@ -18,6 +18,7 @@ import {
   useCofheTokenUnshield,
   useCofheTokenClaimable,
   useCofheTokensWithExistingEncryptedBalances,
+  useTokensWithPublicBalances,
 } from '@/hooks';
 import { useOnceTransactionMined } from '@/hooks/useOnceTransactionMined';
 import { useReschedulableTimeout } from '@/hooks/useReschedulableTimeout';
@@ -107,7 +108,7 @@ function useClaimUnshieldedWithLifecycle() {
 
 type ShieldAndUnshieldViewProps = Omit<ShieldPageViewProps, 'token' | 'mode' | 'setMode'>;
 
-function useShieldWithLifecycle(token: Token): ShieldAndUnshieldViewProps {
+function useShieldWithLifecycle(token: Token): Omit<ShieldAndUnshieldViewProps, 'setToken'> {
   const account = useCofheAccount();
   const [shieldAmount, setShieldAmount] = useState('');
   const { setError, setStatus, error, status } = useLifecycleStore();
@@ -188,7 +189,7 @@ function useShieldWithLifecycle(token: Token): ShieldAndUnshieldViewProps {
   };
 }
 
-function useUnshieldWithLifecycle(token: Token): ShieldAndUnshieldViewProps {
+function useUnshieldWithLifecycle(token: Token): Omit<ShieldAndUnshieldViewProps, 'setToken'> {
   const account = useCofheAccount();
   const { setError, setStatus, error, status } = useLifecycleStore();
   const { schedule: scheduleStatusClear } = useReschedulableTimeout(() => setStatus(null), AUTOCLEAR_TX_STATUS_TIMEOUT);
@@ -265,13 +266,15 @@ function useUnshieldWithLifecycle(token: Token): ShieldAndUnshieldViewProps {
 
 const shieldableTypes = new Set(['dual', 'wrapped']);
 
-export const ShieldPageV2: React.FC<ShieldPageProps> = ({ token, defaultMode }) => {
+export const ShieldPageV2: React.FC<ShieldPageProps> = ({ token: _token, defaultMode }) => {
   const [mode, setMode] = useState<Mode>(defaultMode ?? 'shield');
+  const [overriddenToken, setOverriddenToken] = useState<Token | null>(null);
+  const token = overriddenToken ?? _token;
 
   return mode === 'shield' ? (
-    <ShieldTab token={token} mode={mode} setMode={setMode} />
+    <ShieldTab token={token} mode={mode} setMode={setMode} setToken={setOverriddenToken} />
   ) : (
-    <UnshieldTab token={token} mode={mode} setMode={setMode} />
+    <UnshieldTab token={token} mode={mode} setMode={setMode} setToken={setOverriddenToken} />
   );
 };
 
@@ -280,6 +283,7 @@ type ShieldPageViewProps = {
   token: Token;
   mode: Mode;
   status: { message: string; type: 'info' | 'success' } | null;
+  setToken: (token: Token) => void;
   setMode: (mode: Mode) => void;
 
   isProcessing: boolean;
@@ -297,14 +301,38 @@ type ShieldPageViewProps = {
   primaryIcon: React.ReactNode;
 };
 
-function ShieldTab({ token, mode, setMode }: { token: Token; mode: Mode; setMode: (mode: Mode) => void }) {
+function ShieldTab({
+  token,
+  mode,
+  setMode,
+  setToken,
+}: {
+  token: Token;
+  mode: Mode;
+  setMode: (mode: Mode) => void;
+  setToken: (token: Token) => void;
+}) {
   const shieldViewProps = useShieldWithLifecycle(token);
-  return <ShieldAndUnshieldPageView token={token} mode={mode} setMode={setMode} {...shieldViewProps} />;
+  return (
+    <ShieldAndUnshieldPageView token={token} mode={mode} setMode={setMode} setToken={setToken} {...shieldViewProps} />
+  );
 }
 
-function UnshieldTab({ token, mode, setMode }: { token: Token; mode: Mode; setMode: (mode: Mode) => void }) {
+function UnshieldTab({
+  token,
+  mode,
+  setMode,
+  setToken,
+}: {
+  token: Token;
+  mode: Mode;
+  setMode: (mode: Mode) => void;
+  setToken: (token: Token) => void;
+}) {
   const unshieldViewProps = useUnshieldWithLifecycle(token);
-  return <ShieldAndUnshieldPageView token={token} mode={mode} setMode={setMode} {...unshieldViewProps} />;
+  return (
+    <ShieldAndUnshieldPageView token={token} mode={mode} setMode={setMode} setToken={setToken} {...unshieldViewProps} />
+  );
 }
 
 function ClaimingSection({ token }: { token: Token }) {
@@ -409,10 +437,11 @@ function StatusAndError({
 
 const ShieldAndUnshieldPageView: React.FC<ShieldPageViewProps> = ({
   error,
-  token: _token,
+  token,
   mode,
   setMode,
   status,
+  setToken,
 
   isProcessing,
   inputAmount,
@@ -431,12 +460,10 @@ const ShieldAndUnshieldPageView: React.FC<ShieldPageViewProps> = ({
   const { navigateBack } = usePortalNavigation();
   const { openModal } = usePortalModals();
 
-  const [overriddenToken, setOverriddenToken] = useState<Token | null>(null);
-  const token = overriddenToken ?? _token;
-
   const isShieldableToken = shieldableTypes.has(token.extensions.fhenix.confidentialityType);
 
   const { tokensWithExistingEncryptedBalance } = useCofheTokensWithExistingEncryptedBalances();
+  const { tokensWithPublicBalances } = useTokensWithPublicBalances();
 
   // TODO: probably can be refactored into a view with more stramlined logic
   return (
@@ -456,9 +483,9 @@ const ShieldAndUnshieldPageView: React.FC<ShieldPageViewProps> = ({
               openModal(PortalModal.TokenList, {
                 // TODO: if it's unshield mode, we should only show tokens with existing encrypted balances
                 // if it's shield mode, we need to show all tokens where public balance > 0
-                tokens: tokensWithExistingEncryptedBalance,
+                tokens: mode === 'unshield' ? tokensWithExistingEncryptedBalance : tokensWithPublicBalances,
                 title: mode === 'shield' ? 'Select token to shield' : 'Select token to unshield',
-                onSelectToken: (token) => setOverriddenToken(token),
+                onSelectToken: (token) => setToken(token),
               })
             }
             className="flex items-center gap-1 text-sm font-bold fnx-text-primary hover:opacity-80 transition-opacity"
