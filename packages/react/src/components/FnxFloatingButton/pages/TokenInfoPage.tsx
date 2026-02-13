aeac87c79aa12a7bba40a9b8db37321341a4c8e0
Alexey Shchur <hi@alexeyshchur.com>
2026-02-13 14:35:26 +0200

    tmp: for sepolia, fetch USD data for mainnet WBTC for each token

--- packages/react/src/components/FnxFloatingButton/pages/TokenInfoPage.tsx
+++ packages/react/src/components/FnxFloatingButton/pages/TokenInfoPage.tsx
@@ -3,6 +3,8 @@ import { useCoingeckoContractMarketChartRange } from '@/hooks';
 import { FloatingButtonPage } from '../pagesConfig/types';
 import { usePortalNavigation } from '@/stores';
 import { TokenDetailsView } from '../components/TokenDetailsView';
+import { sepolia } from '@cofhe/sdk/chains';
+import { TMP_WBTC_ON_MAINNET } from '@/utils/coingecko';
 
 type TokenInfoPageProps = {
   token: Token;
@@ -18,10 +20,19 @@ export const TokenInfoPage: React.FC<TokenInfoPageProps> = ({ token }) => {
   const { navigateBack } = usePortalNavigation();
 
   const { data: chartPoints } = useCoingeckoContractMarketChartRange({
-    chainId: token.chainId,
-    contractAddress: token.extensions.fhenix.erc20Pair?.address,
+    ...(token.chainId === sepolia.id
+      ? {
+          chainId: TMP_WBTC_ON_MAINNET.chainId,
+          contractAddress: TMP_WBTC_ON_MAINNET.address,
+        }
+      : {
+          chainId: token.chainId,
+          contractAddress: token.extensions.fhenix.erc20Pair?.address,
+        }),
     rangeMs: 24 * 3600_000,
   });
+
+  console.log('chartPoints', chartPoints);
 
   return <TokenDetailsView token={token} onBack={navigateBack} chartPoints={chartPoints ?? undefined} />;
 };
