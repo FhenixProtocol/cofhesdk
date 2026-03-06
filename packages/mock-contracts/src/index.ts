30fbc7d74e0cc7ab3a5d9f2e9f6eae77f0453895
architect-dev <halftonedev@gmail.com>
2026-03-06 12:04:30 +0100

    Generate and export types from mock-contracts

--- packages/mock-contracts/src/index.ts
+++ packages/mock-contracts/src/index.ts
@@ -4,3 +4,4 @@ export * from './MockACL';
 export * from './MockZkVerifier';
 export * from './MockThresholdNetwork';
 export * from './TestBed';
+export type { MockACL, MockTaskManager, MockThresholdNetwork, MockZkVerifier, TestBed } from './typechain-types';
