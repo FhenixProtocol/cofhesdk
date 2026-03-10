5277245fbbc2d46dfb4ebff1aa0c22cd9e5a05f2
Alexey Shchur <hi@alexeyshchur.com>
2026-03-10 14:08:27 +0200

    chore: cleanup

--- packages/hardhat-plugin-test/test/integration-hardhat.test.ts
+++ packages/hardhat-plugin-test/test/integration-hardhat.test.ts
@@ -1,5 +1,5 @@
 import hre from 'hardhat';
-import { CofheClient, Encryptable, FheTypes, type EncryptedItemInput } from '@cofhe/sdk';
+import { CofheClient, Encryptable, FheTypes } from '@cofhe/sdk';
 import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
 import { expect } from 'chai';
 import { PermitUtils } from '@cofhe/sdk/permits';
