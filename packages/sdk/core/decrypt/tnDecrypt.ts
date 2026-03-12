e8705e4aad2c22b989b8a84b058aaf49ee239cc6
Alexey Shchur <hi@alexeyshchur.com>
2026-03-12 13:23:22 +0200

    fix: be more explicity about which tnDecrypt version to use in decryptForTxBuilder

--- packages/sdk/core/decrypt/tnDecrypt.ts
+++ packages/sdk/core/decrypt/tnDecrypt.ts
@@ -1,5 +1,2 @@
 export { tnDecryptV1 } from './tnDecryptV1.js';
 export { tnDecryptV2 } from './tnDecryptV2.js';
-
-// Backwards-compatible alias: prefer V2 (submit + poll)
-export { tnDecryptV2 as tnDecrypt } from './tnDecryptV2.js';
