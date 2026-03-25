54516b45a54fe7c1ced6b18bac5e3048c6332651
Alexey Shchur <hi@alexeyshchur.com>
2026-03-25 13:03:21 +0200

    doc: move decrypt result verification link to the bottom of decryption section

--- packages/site/vocs.config.ts
+++ packages/site/vocs.config.ts
@@ -81,13 +81,14 @@ export default defineConfig({
                 text: 'Decrypting to Transact',
                 link: '/sdk/decrypt-to-tx',
               },
-              {
-                text: 'Verifying Decrypt Results',
-                link: '/sdk/verify-decrypt-result',
-              },
+
               {
                 text: 'Writing Decrypt Result to Contract',
                 link: '/sdk/writing-decrypt-result-to-contract',
+              },
+              {
+                text: 'Verifying Decrypt Results',
+                link: '/sdk/verify-decrypt-result',
               },
             ],
           },
