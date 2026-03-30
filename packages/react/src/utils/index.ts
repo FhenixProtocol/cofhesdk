cb2a6d3e5f0f2d1ce4cd74d5f181b3c45341104d
Alexey Shchur <hi@alexeyshchur.com>
2026-03-30 14:37:56 +0300

    chore: remove dead code

--- packages/react/src/utils/index.ts
+++ packages/react/src/utils/index.ts
@@ -1,6 +1,5 @@
 export * from './cn';
 export * from './utils';
-export * from './logging';
 export * from './debounce';
 export * from './waitUntilRpcAwareAndReadContract';
 export * from './cofheErrors';
