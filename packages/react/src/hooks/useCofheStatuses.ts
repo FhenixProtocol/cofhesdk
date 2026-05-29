0dd4938f3b468d7648c851daedbfd5f2f44eb920
Alexey Shchur <hi@alexeyshchur.com>
2026-05-29 10:05:16 +0300

    fix: "the result of getSnapshot should be cached" in the consumer

--- packages/react/src/hooks/useCofheStatuses.ts
+++ packages/react/src/hooks/useCofheStatuses.ts
@@ -1,22 +1,27 @@
 import type { CofheStatus } from '@/components/CofheFloatingButton/types';
 import { usePortalStatuses } from '@/stores/portalStatuses';
+import { useMemo } from 'react';
 
 export const useCofheStatuses = (): readonly CofheStatus[] => {
-  return usePortalStatuses((state) =>
-    state.statuses.map((status) => {
-      return {
-        id: status.id,
-        variant: status.variant,
-        title: status.title,
-        description: status.description,
-        action:
-          status.action != null && 'intent' in status.action
-            ? {
-                label: status.action.label,
-                intent: status.action.intent,
-              }
-            : undefined,
-      };
-    })
+  const statuses = usePortalStatuses((state) => state.statuses);
+
+  return useMemo(
+    () =>
+      statuses.map((status) => {
+        return {
+          id: status.id,
+          variant: status.variant,
+          title: status.title,
+          description: status.description,
+          action:
+            status.action != null && 'intent' in status.action
+              ? {
+                  label: status.action.label,
+                  intent: status.action.intent,
+                }
+              : undefined,
+        };
+      }),
+    [statuses]
   );
 };
